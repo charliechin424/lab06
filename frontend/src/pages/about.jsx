f00d75c308c59880cb06ea74f0a9110d15a5d32b
charlie <charlie920424@gmail.com>
2023-04-10 19:25:54 +0800

    lab06

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,3 +1,12 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+  <div>
+    <h1>About me</h1>
+    <ul style={{width: "100%"}}>
+        <li style={{padding: "1%"}}>1. Name : 金家逸</li>
+        <li style={{padding: "1%"}}>2. Student ID : B10502076 </li>
+        <li style={{padding: "1%"}}>3. Department : Sophomore of Electrical Engineering</li>
+        <li style={{padding: "1%"}}>4. Gender : male</li>
+    </ul>
+  </div>)
 }
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -182,13 +182,7 @@ export default function RootLayout() {
 export function RootIndex() {
   return (
     <div className="text">
-      <h1>Welcome!</h1>
-      <ul style={{width: "100%"}}>
-        <li style={{padding: "1%"}}>Name : 金家逸</li>
-        <li style={{padding: "1%"}}>Student ID : B10502076 </li>
-        <li style={{padding: "1%"}}>Department : Sophomore of Electrical Engineering</li>
-        <li style={{padding: "1%"}}>Gender : male</li>
-      </ul>
+      <h1>Welcome my website!</h1>
     </div>
   );
 }

b0a6818b9cd469e6f0810eaa07151090f5349758
charlie <charlie920424@gmail.com>
2023-04-10 19:32:49 +0800

    lab06

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -2,11 +2,11 @@ export default function About() {
   return (
   <div>
     <h1>About me</h1>
-    <ul style={{width: "100%", top: "40%", left: "60%"}}>
-        <li style={{padding: "1%"}}>1. Name : 金家逸</li>
-        <li style={{padding: "1%"}}>2. Student ID : B10502076 </li>
-        <li style={{padding: "1%"}}>3. Department : Sophomore of Electrical Engineering</li>
-        <li style={{padding: "1%"}}>4. Gender : male</li>
+    <ul style={{width: "100%"}}>
+        <li style={{padding: "1%", top: "40%", left: "60%"}}>1. Name : 金家逸</li>
+        <li style={{padding: "1%", top: "40%", left: "60%"}}>2. Student ID : B10502076 </li>
+        <li style={{padding: "1%", top: "40%", left: "60%"}}>3. Department : Sophomore of Electrical Engineering</li>
+        <li style={{padding: "1%", top: "40%", left: "60%"}}>4. Gender : male</li>
     </ul>
   </div>)
 }
